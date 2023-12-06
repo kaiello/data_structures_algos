e0162bc2d8d775125ee4c263499697941a3350d9
Ken Aiello <ken.aiello@gmail.com>
2023-12-06 16:07:57 -0500

    print_list method

--- linked_list.py
+++ linked_list.py
@@ -10,8 +10,37 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
 my_linked_list = LinkedList(4)
 
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
+
+# Print list function. 
+def print_list(self):
+    temp = self.head
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next
+
+# Linked list append method.
+
+def append(self, value):
+    new_node = Node(value)
+    if self.head is None:
+        self.head = new_node
+        self.tail = new_node
+    else:
+        self.tail.next = new_node
+        self.tail = new_node
+    self.length += 1
