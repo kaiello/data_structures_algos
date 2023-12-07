68e361d7bf7ad6f846edbf69b9b686867f87394d
Ken Aiello <ken.aiello@gmail.com>
2023-12-07 15:25:50 -0500

    update

--- linked_list.py
+++ linked_list.py
@@ -27,15 +27,37 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self, value):
+        if self.head is None and self.tail is None:
+            return None
+        elif self.head == self.tail:
+            pop_value = value
+            self.remove(value)
+            return pop_value
+        # else:
+        #     temp = self.head
+        #     while temp.next != self.tail:
+        #         temp = temp.next
+        #     temp.next = None
+        #     self.tail = temp
+        #     self.length -= 1
+        #     return temp
 
-my_linked_list = LinkedList(4)
+
+
+# my_linked_list = LinkedList(4)
 
 # print('Head:', my_linked_list.head.value)
 # print('Tail:', my_linked_list.tail.value)
 # print('Length:', my_linked_list.length)
 
-my_linked_list = LinkedList(1)
+my_linked_list = LinkedList.(1)
 
-my_linked_list.append(2)
+# my_linked_list = LinkedList(1)
 
-my_linked_list.print_list()
+# my_linked_list.append(2)
+
+# my_linked_list.print_list()
+
+my_linked_list.pop(1)
